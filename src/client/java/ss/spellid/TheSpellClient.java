c77e8ca45f05ace957c8daa144b37cf5f5b74b7d
Pennsann <105668713+Pennsann@users.noreply.github.com>
2026-02-15 21:37:06 +0200

    Initial commit

---
+++ src/client/java/ss/spellid/TheSpellClient.java
@@ -0,0 +1,10 @@
+package ss.spellid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheSpellClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/ss/spellid/TheSpell.java
@@ -0,0 +1,16 @@
+package ss.spellid;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TheSpell implements ModInitializer {
+	public static final String MOD_ID = "the-spell";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
