d651ba13fc2a9bb23cfb1736b4c0d3ebccf97c24
Pennsann <105668713+Pennsann@users.noreply.github.com>
2026-02-16 16:13:45 +0200

    okay I am lazy to make the rest :P

---
+++ src/main/java/ss/spellid/item/DormantFragment.java
@@ -0,0 +1,36 @@
+package ss.spellid.item;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class DormantFragment extends Item {
+
+    public DormantFragment(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResult use(Level level, Player user, InteractionHand hand) {
+        //This is the client side of things
+        if (level.isClientSide()) {
+            return InteractionResult.PASS;
+        }
+
+        ItemStack stack = user.getItemInHand(hand);
+        if(!stack.isEmpty()) {
+            stack.shrink(1);
+
+            if (stack.isEmpty()) {
+                user.setItemInHand(hand, ItemStack.EMPTY);
+            }
+            //Change this later to fill essence into the core
+            user.giveExperienceLevels(12);
+
+        }
+        return InteractionResult.SUCCESS;
+    }
+}
--- src/main/java/ss/spellid/item/ModItems.java
+++ src/main/java/ss/spellid/item/ModItems.java
@@ -1,10 +1,8 @@
 package ss.spellid.item;
 
-import net.fabricmc.fabric.api.item.v1.FabricItem;
 import java.util.function.Function;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
@@ -12,16 +10,14 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import org.slf4j.LoggerFactory;
 import ss.spellid.TheSpell;
 
 public class ModItems {
 
     public static final Item dormantSoulFragment = register("dormant_soul_fragment",
-            Item::new, new Item.Properties().stacksTo(16));
+            DormantFragment::new, new Item.Properties().stacksTo(16));
 
     public static final ResourceKey<CreativeModeTab> CUSTOM_CREATIVE_TAB_KEY = ResourceKey.create(BuiltInRegistries
             .CREATIVE_MODE_TAB.key(), Identifier.fromNamespaceAndPath(TheSpell.MOD_ID, "creative_tab"));
